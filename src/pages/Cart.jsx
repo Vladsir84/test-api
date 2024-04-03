1721fc485847811f1a24592d446a25d48640a124
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2024-04-03 08:44:54 +0300

    Update Cart.jsx

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -6,13 +6,44 @@ const Cart = () => {
     JSON.parse(localStorage.getItem("cart")) || []
   );
 
+  const handleAdd = (product) => {
+    const cart = JSON.parse(localStorage.getItem("cart")) || [];
+    const indexToAdd = cart.findIndex((el) => el.id === product.id);
+    if (indexToAdd === -1) {
+      const newArr = [
+        ...cart,
+        {
+          id: product.id,
+          title: product.title,
+          parent_id: product.parent_id,
+          quantity: 1,
+        },
+      ];
+      localStorage.setItem("cart", JSON.stringify(newArr));
+      setCartList(newArr);
+    } else {
+      const updatedCart = cart.map((item) => {
+        if (item.id === product.id) {
+          return { ...item, quantity: item.quantity + 1 };
+        }
+        return item;
+      });
+      localStorage.setItem("cart", JSON.stringify(updatedCart));
+      setCartList(updatedCart);
+    }
+  };
+
   const handleRemove = (id) => {
     const cart = JSON.parse(localStorage.getItem("cart")) || [];
     const indexToRemove = cart.findIndex((el) => el.id === id);
 
     if (indexToRemove !== -1) {
       const updatedCart = [...cart];
-      updatedCart.splice(indexToRemove, 1);
+      if (updatedCart[indexToRemove].quantity > 1) {
+        updatedCart[indexToRemove].quantity -= 1;
+      } else {
+        updatedCart.splice(indexToRemove, 1);
+      }
 
       setCartList(updatedCart);
       localStorage.setItem("cart", JSON.stringify(updatedCart));
@@ -28,9 +59,21 @@ const Cart = () => {
         {cartList.map((item, index) => (
           <div key={index} className="cart-item">
             <div>{item.title}</div>
-            <span className="delete-icon" onClick={() => handleRemove(item.id)}>
+            <div className="btn-icon-wrapper ">
+              <span className="btn-icon" onClick={() => handleAdd(item)}>
+                +
+              </span>
+            </div>
+            <span
+              className="btn-icon delete"
+              onClick={() => handleRemove(item.id)}
+            >
               +
             </span>
+            <p style={{ marginLeft: "10px" }}>
+              Количество товара:{" "}
+              <span style={{ color: "blue" }}>{item.quantity}</span>
+            </p>
           </div>
         ))}
       </div>
