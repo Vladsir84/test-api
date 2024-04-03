e2e8e407a344268f3c28afadcc1083a3b802a384
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2024-04-03 08:46:29 +0300

    Update Product.jsx

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,22 +1,43 @@
 import { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 
-const Product = ({ goods }) => {
+const Product = () => {
   const params = useParams();
   const [product, setProduct] = useState(null);
 
   useEffect(() => {
-    goods.forEach((el) => {
-      if (el?.data?.id?.includes(params.id)) {
-        setProduct(el?.data);
-      }
-    });
-  }, [goods, params]);
+    const storedProduct = localStorage.getItem("product");
+    if (storedProduct) {
+      setProduct(JSON.parse(storedProduct));
+    }
+  }, []);
 
   const handleAdd = () => {
     const cart = JSON.parse(localStorage.getItem("cart")) || [];
-    const newArr = [...cart, product];
-    localStorage.setItem("cart", JSON.stringify(newArr));
+    const existingProductIndex = cart.findIndex(
+      (item) => item.id === product.data.id
+    );
+
+    if (existingProductIndex !== -1) {
+      const updatedCart = cart.map((item, index) => {
+        if (index === existingProductIndex) {
+          return { ...item, quantity: item.quantity + 1 };
+        }
+        return item;
+      });
+      localStorage.setItem("cart", JSON.stringify(updatedCart));
+    } else {
+      const newArr = [
+        ...cart,
+        {
+          id: product.data.id,
+          title: product.data.title,
+          parent_id: product.data.parent_id,
+          quantity: 1,
+        },
+      ];
+      localStorage.setItem("cart", JSON.stringify(newArr));
+    }
   };
 
   const handleRemove = () => {
@@ -25,7 +46,13 @@ const Product = ({ goods }) => {
 
     if (indexToRemove !== -1) {
       const updatedCart = [...cart];
-      updatedCart.splice(indexToRemove, 1);
+      const itemToRemove = updatedCart[indexToRemove];
+
+      if (itemToRemove.quantity > 1) {
+        itemToRemove.quantity -= 1;
+      } else {
+        updatedCart.splice(indexToRemove, 1);
+      }
 
       localStorage.setItem("cart", JSON.stringify(updatedCart));
     }
@@ -37,13 +64,13 @@ const Product = ({ goods }) => {
         <p>{"< "}Back </p>
       </Link>
       <div className="product-wrapper">
-        <h1 style={{ marginTop: "0px" }}>{product?.title}</h1>
-        <p dangerouslySetInnerHTML={{ __html: product?.description }} />
+        <h1 style={{ marginTop: "0px" }}>{product?.data?.title}</h1>
+        <p dangerouslySetInnerHTML={{ __html: product?.data?.description }} />
         <div className="product-chars">
           <div>
             <h2>Характеристики:</h2>
             {product &&
-              Object.values(product?.characteristics).map((item) => (
+              Object.values(product?.data?.characteristics).map((item) => (
                 <div key={item?.id} className="chars-wrapper">
                   <p>{item?.title}:</p>
                   <p style={{ marginLeft: "20px" }}>{item?.value}</p>
@@ -57,10 +84,14 @@ const Product = ({ goods }) => {
               }}
             >
               <h2>Цена:</h2>
-              <p style={{ marginLeft: "20px" }}>{product?.price}</p>
+              <p style={{ marginLeft: "20px" }}>{product?.data?.price}</p>
             </div>
           </div>
-          <img src={product?.images?.[0]} alt="" className="product-image" />
+          <img
+            src={product?.data?.images?.[0]}
+            alt=""
+            className="product-image"
+          />
         </div>
 
         <div className="btns-wrapper">
